6dc64a513b3d7dd78dad70939212272f71d93273
Marijan Svalina <marijan.svalina@gmail.com>
2014-09-22 07:56:10 +0200

    admin.py > added better presentation of models in admin

--- ryu/kurama/admin.py
+++ ryu/kurama/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from kurama.models import TaskList, Task, Project
 
-admin.site.register(TaskList)
+class TaskListAdmin(admin.ModelAdmin):
+    """
+    Changes admin options and functionality for given model
+    """
+    fields = ('title', 'updated', 'taskListId', 'selfLink')
+    list_display = ('title', 'updated')
+
+admin.site.register(TaskList, TaskListAdmin)
 
 class TaskAdmin(admin.ModelAdmin):
-    fields = ('title', 'tag', 'completed')
+    """
+    Changes admin options and functionality for given model
+    """
+    fields = ('title', 'tag', 'completed', 'notes')
+    list_display = ('title', 'tag', 'completed')
 
 admin.site.register(Task, TaskAdmin)
 
-admin.site.register(Project)
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    """
+    Changes admin options and functionality for given model
+    """
+    fields = ('name', 'tag', 'description', 'status', 'due', 'completed')
+    list_display = ('name', 'tag', 'completed')
+
+admin.site.register(Project, ProjectAdmin)
