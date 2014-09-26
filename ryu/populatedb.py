e481b0070bfb0090bd655969d34212de7b764c13
Marijan Svalina <marijan.svalina@gmail.com>
2014-09-26 06:40:47 +0200

    populatedb.py > some pylinting

--- ryu/populatedb.py
+++ ryu/populatedb.py
@@ -10,42 +10,47 @@ from apiclient.discovery import build
 from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run
-from datetime import date, timedelta
+from datetime import date
 from kurama.models import TaskList, Task, Project
+
+FLAGS = gflags.FLAGS
 
 class PopulateDB(object):
 
-    clientID = ""
-    clientSecret = ""
-    apiKey = ""
+    client_id = ""
+    client_secret = ""
+    api_key = ""
     service = None
 
     def __init__(self):
         if os.path.isfile("tasks.dat"):
-            self.getService()
-            self.setEnv()
+            self.get_service()
+            self.set_env()
         else:
             try:
-                with open("keys.txt", "r") as self.f:
-                    self.clientID = self.f.readline().rstrip()
-                    self.clientSecret = self.f.readline().rstrip()
-                    self.apiKey = self.f.readline().rstrip()
+                with open("keys.txt", "r") as kfile:
+                    self.client_id = kfile.readline().rstrip()
+                    self.client_secret = kfile.readline().rstrip()
+                    self.api_key = kfile.readline().rstrip()
             except IOError:
-                self.clientID = raw_input("Enter your client id: ")
-                self.clientSecret = raw_input("Enter your client secret: ")
-                self.apiKey = raw_input("Enter your api key: ")
-                self.writeAuth()
-            self.getService()
-            self.setEnv()
+                self.client_id = raw_input("Enter your client id: ")
+                self.client_secret = raw_input("Enter your client secret: ")
+                self.api_key = raw_input("Enter your api key: ")
+                self.write_auth()
+            self.get_service()
+            self.set_env()
 
-    def writeAuth(self):
-        with open("keys.txt", "w") as self.fw:
-            self.fw.write(str(self.clientID) + "\n")
-            self.fw.write(str(self.clientSecret) + "\n")
-            self.fw.write(str(self.apiKey) + "\n")
+    def write_auth(self):
+        """ Writing entered clinet_id, client_secret and api_key """
+        with open("keys.txt", "w") as kfile:
+            kfile.write(str(self.client_id) + "\n")
+            kfile.write(str(self.client_secret) + "\n")
+            kfile.write(str(self.api_key) + "\n")
 
-    def getService(self):
-        FLAGS = gflags.FLAGS
+    def get_service(self):
+        """ TODO shift this function outside of PopulateDB bc it does not make
+        sense to be a part of populateDB but it makes sense to use it in
+        __init__ """
 
         # Set up a Flow object to be used if we need to authenticate. This
         # sample uses OAuth 2.0, and we set up the OAuth2WebServerFlow with
@@ -54,9 +59,9 @@ class PopulateDB(object):
         # applications
         # The client_id and client_secret are copied from the API Access tab on
         # the Google APIs Console
-        FLOW = OAuth2WebServerFlow(
-            client_id = self.clientID,
-            client_secret = self.clientSecret,
+        flow = OAuth2WebServerFlow(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
             scope='https://www.googleapis.com/auth/tasks',
             user_agent='PopulateDB/0.0.1')
 
@@ -69,7 +74,7 @@ class PopulateDB(object):
         storage = Storage('tasks.dat')
         credentials = storage.get()
         if credentials is None or credentials.invalid == True:
-            credentials = run(FLOW, storage)
+            credentials = run(flow, storage)
 
         # Create an httplib2.Http object to handle our HTTP requests and
         # authorize it with our good Credentials.
@@ -81,33 +86,34 @@ class PopulateDB(object):
         # to get a developerKey for your own application.
         # I used API key for Browser applications
         self.service = build(serviceName='tasks', version='v1', http=http,
-                             developerKey=self.apiKey)
+                             developerKey=self.api_key)
 
-    def setEnv(self):
-        # Seting python and djagno env var
-        ryuDir = os.path.dirname(os.path.realpath(__file__))
-        sys.path.append(ryuDir)
+    def set_env(self):
+        """ Seting python and django env var """
+        ryu_dir = os.path.dirname(os.path.realpath(__file__))
+        sys.path.append(ryu_dir)
         os.environ['DJANGO_SETTINGS_MODULE'] = 'ryu.settings'
 
-    def getTaskLists(self):
-        tagDict = self.getProjectLists()
-        taskLists = self.service.tasklists().list().execute()
-        # Save every tasklist that's in usedLists to database
-        usedLists = ['01-Im&Ds', '02-Im&Nds', '03-Ni&Ds', '04-Ni&Nds']
-        for tl in taskLists['items']:
-            if tl['title'] in usedLists:
+    def get_task_lists(self):
+        """ Fetch task lists from tasks api and save them in django model """
+        tag_dict = self.get_project_lists()
+        task_lists = self.service.tasklists().list().execute()
+        # Save every tasklist that's in used_lists to database
+        used_lists = ['01-Im&Ds', '02-Im&Nds', '03-Ni&Ds', '04-Ni&Nds']
+        for tskl in task_lists['items']:
+            if tskl['title'] in used_lists:
                 print "Saving task list: "
-                print tl['title']
-                listEntry = TaskList(taskListId=tl['id'],
-                                      title=tl['title'],
-                                      updated=tl['updated'],
-                                      selfLink=tl['selfLink'])
+                print tskl['title']
+                listEntry = TaskList(taskListId=tskl['id'],
+                                      title=tskl['title'],
+                                      updated=tskl['updated'],
+                                      selfLink=tskl['selfLink'])
                 listEntry.save()
 
                 # Get completed tasks from January first 2014 until today
                 today = date.today().strftime('%Y-%m-%dT00:00:00Z')
                 tasks = self.service.tasks().list(
-                            tasklist=tl['id'],
+                            tasklist=tskl['id'],
                             completedMin="2014-01-01T00:00:00Z",
                             completedMax=today,
                             showHidden="True").execute()
@@ -115,25 +121,25 @@ class PopulateDB(object):
                 for tsk in tasks['items']:
                     # Set default task tag to *raz* in case there is no tag in
                     # Project List
-                    taskTag = "*raz*"
-                    taskTitle = tsk['title'].strip()
+                    task_tag = "*raz*"
+                    task_title = tsk['title'].strip()
                     print tsk['title']
 
-                    # Get task tag from tagDict insted of directly spliting from
-                    # task name, so now task which has tag without space before
-                    # title can be recongnized correctly
-                    for tag in tagDict:
+                    # Get task tag from tag_dict insted of directly spliting
+                    # from task name, so now task which has tag without space
+                    # before title can be recongnized correctly
+                    for tag in tag_dict:
                         if tag in tsk['title']:
-                            taskTag = tag
-                            tagName = tagDict[tag]
-                            taskTitle = string.replace(tsk['title'], tag, "")
-                            taskTitle = taskTitle.strip()
+                            task_tag = tag
+                            tag_name = tag_dict[tag]
+                            task_title = string.replace(tsk['title'], tag, "")
+                            task_title = task_title.strip()
                     # Save fetched task in corresponding db field
                     taskEntry = Task(taskList=listEntry,
                                      taskId=tsk['id'],
-                                     tag=taskTag,
-                                     tagName=tagName,
-                                     title=taskTitle,
+                                     tag=task_tag,
+                                     tagName=tag_name,
+                                     title=task_title,
                                      updated=tsk['updated'],
                                      selfLink=tsk['selfLink'],
                                      parent=tsk.get('parent', None),
@@ -144,13 +150,15 @@ class PopulateDB(object):
                                      completed=tsk['completed'])
                     taskEntry.save()
 
-    def getProjectLists(self):
-        taskLists = self.service.tasklists().list().execute()
-        tagDict = {}
-        for tl in taskLists['items']:
-            if tl['title'] == "Projects list":
-                print tl['title']
-                tasks = self.service.tasks().list(tasklist=tl['id']).execute()
+    def get_project_lists(self):
+        """ Fetch Project List from task api, save it to model and return list
+        of tags with full names in dictionary """
+        task_lists = self.service.tasklists().list().execute()
+        tag_dict = {}
+        for tskl in task_lists['items']:
+            if tskl['title'] == "Projects list":
+                print tskl['title']
+                tasks = self.service.tasks().list(tasklist=tskl['id']).execute()
                 for tsk in tasks['items']:
                     tag = string.split(tsk['title'], "-")[0].strip()
                     title = string.split(tsk['title'], "-")[1].strip()
@@ -163,13 +171,13 @@ class PopulateDB(object):
                         due=tsk.get('due', None),
                         completed=tsk.get('completed', None))
                     projectEntry.save()
-                    tagDict[tag] = title
+                    tag_dict[tag] = title
                     print tsk['title']
-        return tagDict
+        return tag_dict
 
 def main():
     populateDB = PopulateDB()
-    populateDB.getTaskLists()
+    populateDB.get_task_lists()
 
 if __name__ == '__main__':
     main()
