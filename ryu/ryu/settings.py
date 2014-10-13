03475749317f12c7b0a651edb8de44ef32e6da14
Marijan Svalina <marijan.svalina@gmail.com>
2014-10-13 07:27:16 +0200

    Added django-debug-toolbar, how could I worked without it!

--- ryu/ryu/settings.py
+++ ryu/ryu/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'south',
     'bootstrap3',
     'djangobower',
